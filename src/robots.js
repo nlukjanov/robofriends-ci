456863af4e4e1eff4bd3659685843175ba35461d
Nikita <nlukjanov@gmail.com>
2018-08-11 12:11:49 +0100

    added robots.js

---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'sincere@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Elvin Howerl',
+    username: 'El',
+    email: 'efi@apriloo.biz'
+  },
+  {
+    id: 3,
+    name: 'Clement Jones',
+    username: 'Jo',
+    email: 'sinceadre@april.biz'
+  },
+  {
+    id: 4,
+    name: 'Patrick Lowerl',
+    username: 'Pat',
+    email: 'sincsadfe@april.biz'
+  },
+  {
+    id: 5,
+    name: 'Chelsea Grammer',
+    username: 'Chel',
+    email: 'chel@april.biz'
+  },
+]
