cc407d537280e96f6abf5aaf9eeee6a0aad9f1df
Nikita <nlukjanov@gmail.com>
2018-08-12 12:33:33 +0100

    added filter function

--- src/App.js
+++ src/App.js
@@ -11,11 +11,18 @@ class App extends Component {
       searchfield: ''
     }
   }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    })
+  }
   render() {
     return (
       <div className='tc'>
         <h1>RoboFriends</h1>
-        <SearchBox />
+        <SearchBox searchChange={this.onSearchChange}/>
         <CardList robots={this.state.robots} />
       </div>
     );
