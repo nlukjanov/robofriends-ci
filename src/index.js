ae7c84646c57801f4fc47c8b7c9bc6869b02b72a
Nikita Lukjanov <nlukjanov@gmail.com>
2018-08-13 19:29:28 +0100

    added Provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './containers/App';
+import { searchRobots } from './reducers';
 import './index.css';
 import 'tachyons';
 
+const store = createStore(searchRobots);
+
 
 ReactDOM.render(
-  <App />
-  , document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root')
 );
+
