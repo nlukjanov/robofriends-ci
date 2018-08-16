a57bfceabbd670ea527a1cd5e64f2096082046b1
Nikita Lukjanov <nlukjanov@gmail.com>
2018-08-16 09:56:46 +0100

    installed middleware

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import App from './containers/App';
 import { searchRobots } from './reducers';
 import './index.css';
 import 'tachyons';
 
-const store = createStore(searchRobots);
-
+const logger = createLogger;
+const store = createStore(searchRobots, applyMiddleware(logger));
 
 ReactDOM.render(
   <Provider store={store}>
