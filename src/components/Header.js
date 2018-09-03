dd8cd10d5c9d137d1d496725c22dafac931ce5f0
Nikita Lukjanov <nlukjanov@gmail.com>
2018-09-03 09:48:31 +0100

    removed counter

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import CounterButton from './CounterButton';
 
 class Header extends Component {
   // shouldComponentUpdate(nextProps, nextState) {
@@ -11,7 +10,6 @@ class Header extends Component {
     return (
       <div>
         <h1 className='ma2 f-subheadline lh-title'>RoboFriends</h1>
-        <CounterButton color={'red'} />
       </div>
     );
   }
