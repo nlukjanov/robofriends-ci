5918f393c6a233070d02c0c52031eb7c26d1c4dc
Nikita Lukjanov <nlukjanov@gmail.com>
2018-09-18 10:35:28 +0100

    added service worker

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import App from './containers/App';
+import registerServiceWorker from './registerServiceWorker';
 import { searchRobots, requestRobots } from './reducers';
 import './index.css';
 import 'tachyons';
@@ -13,10 +14,11 @@ import 'tachyons';
 const logger = createLogger();
 
 const rootReducer = combineReducers({ searchRobots, requestRobots })
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>, document.getElementById('root')
 );
+registerServiceWorker();
