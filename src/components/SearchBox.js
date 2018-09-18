3a59e010226e1b3e6d19f799589b482a6cfa850b
Nikita Lukjanov <nlukjanov@gmail.com>
2018-09-18 11:47:12 +0100

    added label to search form for accessibility

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,8 +4,10 @@ const SearchBox = ({ searchfield, searchChange }) => {
   console.log('SearchBox')
   return (
     <div className='pa2'>
+      <label htmlFor="search">Search Robots</label>
       <input
         className='pa3 ba b--green bg-lightest-blue'
+        id="search"
         type="search"
         placeholders='search robots'
         onChange={searchChange}
